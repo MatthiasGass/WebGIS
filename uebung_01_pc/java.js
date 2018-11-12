67af078dc582d8e11c2b1e09dc1bc77756aa8327
Gassilloud <in227@stud>
2018-11-12 14:58:31 +0100

    update pc

---
+++ uebung_01_pc/java.js
@@ -0,0 +1,15 @@
+
+let x = 10;
+let y = 10;
+
+
+
+function Test(x,y){
+  var z = x*y
+  return z
+};
+
+var Ergebnis = Test(x,y)
+console.log(Ergebnis)
+
+let Text1 = document.getElementById('1').value
