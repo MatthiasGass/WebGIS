f759943091855adf7664e126e419e03cec4527ce
mg <matthias.gass@gmx.de>
2018-11-17 21:39:10 +0100

    update

--- uebung_01_pc/java.js
+++ uebung_01_pc/java.js
@@ -1,22 +1,53 @@
 
 
 
-let geoJson = {
-  "type": "FeatureCollection",
-  features":[],
+
+function testf() {
+
+// Werte aus Formular auslesen
+  let poi_name = document.getElementById("name").value;
+  let poi_typ = document.getElementById("typ").value;
+  let poi_gb = document.getElementById("gb").value;
+  let poi_gl = document.getElementById("gl").value;
+  let poi_beschr = document.getElementById("beschr").value;
+
+// geoJson erstellen
+  let geoJson = {
+    "type": "FeatureCollection",
+    "features":[],
+  };
+
+// feature erstellen
+  let feature = {
+   "type": "Feature",
+   "properties": {},
+   "geometry":{
+      "type": "Point",
+      "coordinates":[],
+    }
+  };
+
+
+// Ausgelesene Werte in das Feature schreiben
+feature.properties.name = poi_name;
+let punkt = [parseFloat(poi_gb), parseFloat(poi_gl)];
+feature.geometry.coordinates = punkt;
+
+
+// Feature der Feature Collection hinzufügen
+geoJson.features.push(feature);
+z = JSON.stringify(geoJson);
+
+
+
+
 };
 
-// funktion bei button klick{}
-let feature = {
-  "type" = "Feature",
-  "properties": {},
-  "geometry":{
-    "type":"Point",
-    "coordinates":[],
-  }
+
+test2() {
+document.getElementById("input").innerHTML = "1abc";
 };
 
-
-// funktionen zum hinzufügen
-
-// funktion bei button klick ENDE
+// GeoJSON anzeigen
+// console.log(JSON.stringify(geoJson));
+// console.log(z)
--- uebung_01_pc/script.js
+++ uebung_01_pc/script.js
@@ -1,1 +1,6 @@
-"use strict"
+// test
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("test").innerHTML = "123";
+    document.getElementById("test2").innerHTML = "1abc";
+}
