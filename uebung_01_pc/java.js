eba5391559dd9ffe576127e4a09294cb4969100c
mg <matthias.gass@gmx.de>
2018-11-20 18:12:35 +0100

    update

--- uebung_01_pc/java.js
+++ uebung_01_pc/java.js
@@ -1,4 +1,4 @@
-// object geoJson erstellen
+// globales object geoJson erstellen
 let geoJson = {
   "type": "FeatureCollection",
   "features":[],
@@ -6,16 +6,16 @@ let geoJson = {
 
 
 // erstellt die Funktion testf die mit dem Button "Submit" ausgeführt wird
-function testf() {
+function func1() {
 
-// Werte werden aus dem Formular ausgelesen
+  // Werte werden aus dem Formular über eine ID ausgelesen und variablen zugeordnet
   let poi_name = document.getElementById("name").value;
   let poi_typ = document.getElementById("typ").value;
   let poi_gb = document.getElementById("gb").value;
   let poi_gl = document.getElementById("gl").value;
   let poi_beschr = document.getElementById("beschr").value;
 
-// object feature erstellen
+  // object feature erstellen
   let feature = {
    "type": "Feature",
    "properties": {},
@@ -25,39 +25,59 @@ function testf() {
     }
   };
 
-  // Ausgelesene Werte in das Feature schreiben
-  feature.properties.name = poi_name;
-  feature.properties.typ = poi_typ;
-  feature.properties.beschreibung = poi_beschr;
+  // Ausgelesene Werte des Formulars werden in das Feature geschrieben
+  feature.properties.Name = poi_name;
+  feature.properties.Typ = poi_typ;
+  feature.properties.Beschreibung = poi_beschr;
+
+  // Koordinaten werden in einem Array zusammengefasst und in das Feature geschrieben
   let punkt = [parseFloat(poi_gb), parseFloat(poi_gl)];
   feature.geometry.coordinates = punkt;
 
-  // Feature der Feature Collection hinzufügen
+  // Feature wird der Feature Collection des GeoJsons hinzugefügt
   geoJson.features.push(feature);
 
-  // stringify geojson
-  geoJson_string = JSON.stringify(geoJson); // stringify zusätzliche argumente für ordentlichkeit
+  // stringify geojson zur Textausgabe in HTML
+  geoJson_string = JSON.stringify(geoJson, null, ' '); // stringify zusätzliche argumente für ordentlichkeit
 
   // Ausgabe des Geojsons im HTML Dokument
-  document.getElementById("Ausgabe").innerHTML = geoJson_string;
+  document.getElementById("Ausgabe_wgs").innerHTML = geoJson_string;
 
 };
 
-function neu() {
-  let input = document.getElementById("Ausgabe").value;
-  gj_alt = JSON.parse(input);
 
-  gj_alt.crs = {};
+// erstellt die Funktion tneu() die mit dem Button "Submit" ausgeführt wird
+function func2() {
 
-  // Schleife
-  for(let i = 0; i < gj_alt.features.length; i++) {
-    feature = jg_alt.features[i]
-    coordinaten = feature.coordinates
-    xcoordinate = coordinaten[0]
-    ycoordinate = coordinaten[1]
+  // Werte aus dem Textausgabefeld werden eingelesen und eienr Variable zugeordnet
+  let input = document.getElementById("Ausgabe_wgs").value;
 
-    x = lon * 20037508.34 / 180
+  // konvertieren des geoJsons in ein Javascript Object und Zuordnung zu einer neuen Variable
+  gj_neu = JSON.parse(input);
+
+  // hinzufügen der crs Information (Projektionsinformation)
+  gj_neu.crs = {
+    "type": "name",
+    "properties": { "name": "urn:ogc:def:crs:EPSG::3857" }
+  };
+
+  // Schleife ruft jedes Feature des geoJson einzeln ab, extrahiert die Koordinaten...
+  for(let i = 0; i < gj_neu.features.length; i++) {
+    let feature = gj_neu.features[i];
+    let coordinaten = feature.geometry.coordinates;
+    let x_wgs = coordinaten[0];
+    let y_wgs = coordinaten[1];
+
+    // projiziert die Koordinaten von WGS84 nach WEBMercator durch folgende Formel:
+    let x_wm = x_wgs * 20037508.34 / 180;
+    let y_wm = (Math.log(Math.tan((90 + y_wgs) * Math.PI / 360.0 )) / (Math.PI / 180.0)) * 20037508.34 / 180;
+
+    // Zusammenfassen der Koordinaten in einem Array, Koordinaten in Feature schreiben
+    let punkt_wm = [parseFloat(x_wm), parseFloat(y_wm)];
+    feature.geometry.coordinates = punkt_wm;
+
   }
 
-
+  geoJson_string = JSON.stringify(gj_neu, null, ' ');
+  document.getElementById("Ausgabe_wm").innerHTML = geoJson_string;
 };
